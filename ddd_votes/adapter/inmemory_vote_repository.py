0758cdf02e1db3ff78b23dd48142bcfdfc776046
Viswas Vuppala <viswas@steelbasis.com>
2022-06-09 19:59:24 -0400

    added inmemory_vote_repository implementing the vote repository and 3 new passing tests

--- ddd_votes/adapter/inmemory_vote_repository.py
+++ ddd_votes/adapter/inmemory_vote_repository.py
@@ -1,2 +1,19 @@
 ## This is the implmentation of the vote_repository
-## It 
+from typing import List
+from ddd_votes.domain.vote import Vote
+from ddd_votes.domain.vote_repository import VoteRepository
+
+
+class InMemoryVoteRepository(VoteRepository):
+    def __init__(self):
+        self.votes = []
+
+    def add(self, vote: Vote) -> Vote:
+        self.votes.append(vote)
+        return vote
+
+    def all(self) -> List[Vote]:
+        return self.votes
+
+    def total(self) -> int:
+        return len(self.votes)
---
+++ tests/adapter/test_inmemory_vote_repository.py
@@ -0,0 +1,23 @@
+from ddd_votes.adapter.inmemory_vote_repository import InMemoryVoteRepository
+from ddd_votes.domain.vote import Vote
+
+def test_vote_save():
+    vote = Vote()
+    vote_repository = InMemoryVoteRepository()
+
+    assert vote.save(vote_repository).vote_id == vote.vote_id
+
+def test_vote_repository_all():
+    vote_repository = InMemoryVoteRepository()
+    vote1 = Vote().save(vote_repository)
+    vote2 = Vote().save(vote_repository)
+
+    assert set(vote_repository.all()) == {vote1, vote2}
+
+def test_vote_repository_total():
+    vote_repository = InMemoryVoteRepository()
+    Vote().save(vote_repository)
+    Vote().save(vote_repository)
+
+    assert vote_repository.total() == 2
+
