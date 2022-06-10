d83a13be0ab4dae9bfe0ef3b165f24d094d2cea5
Viswas Vuppala <viswas@steelbasis.com>
2022-06-10 16:44:13 -0400

    added fast api and uvicorn to get quick swagger docs. Functionality not added yet

--- ddd_votes/main.py
+++ ddd_votes/main.py
@@ -1,3 +1,11 @@
 from ddd_votes.adapter.inmemory_vote_repository import InMemoryVoteRepository
 from ddd_votes.domain.vote import Vote
+from fastapi import FastAPI
 
+app = FastAPI()
+
+vote_repository = InMemoryVoteRepository()
+
+@app.get("/")
+def root():
+    return {"Message": "Testing the Start"}
