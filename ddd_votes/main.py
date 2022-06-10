240d387cd67b0d26ce0e52565026adabcae91349
Viswas Vuppala <viswas@steelbasis.com>
2022-06-10 17:12:20 -0400

    added api POST fucntion to create vote and 2 get functions to check on the votes

--- ddd_votes/main.py
+++ ddd_votes/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from ddd_votes.adapter.inmemory_vote_repository import InMemoryVoteRepository
 from ddd_votes.domain.vote import Vote
 from fastapi import FastAPI
@@ -9,3 +10,15 @@ vote_repository = InMemoryVoteRepository()
 @app.get("/")
 def root():
     return {"Message": "Testing the Start"}
+
+@app.post("/vote", response_model=Vote)
+def create_vote():
+    return Vote().save(vote_repository)
+
+@app.get('/votes', response_model= int)
+def get_total_votes():
+    return vote_repository.total()
+
+@app.get('/vote_list', response_model= List)
+def get_votes_list():
+    return vote_repository.all()
